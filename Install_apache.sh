6e507537457c8163db6c3f78c61b1816d2499b7f
ManideepReddy3533 <103239984+ManideepReddy3533@users.noreply.github.com>
2024-01-27 09:46:29 +0530

    Added the file to git

---
+++ Install_apache.sh
@@ -0,0 +1,11 @@
+echo "update dependencies and libraries"
+suo apt update
+echo"All the dependencies and libraries required are created"
+
+echo"apt a packet manager that install's apache server"
+sudo apt install apache2
+echo" Server installed"
+
+echo"to check the server status"
+sudo systemctl status apache2
+echo"Status is displayed"
